740ec7029e5c918b1f10bf4509d2d331b35b596d
LuisRosaless <wicholr98@gmail.com>
2021-11-27 17:32:04 -0600

    Version1.25

--- app/src/main/java/com/example/proyectomc_dispmov/MainActivity.kt
+++ app/src/main/java/com/example/proyectomc_dispmov/MainActivity.kt
@@ -5,28 +5,30 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.proyectomc_dispmov.databinding.ActivityMainBinding
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registro)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         auth= Firebase.auth
 
-        Ingresar.setOnClickListener {
-            val intent: Intent = Intent(this, Autentificacion::class.java)
+        Ingres.setOnClickListener {
+            val intent  = Intent(this,Autentificacion::class.java)
             startActivity(intent)
+
         }
 
     }
