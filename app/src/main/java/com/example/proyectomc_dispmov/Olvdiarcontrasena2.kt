dd71004cf00d24c82a3ea977b788735b4654f785
LuisRosaless <wicholr98@gmail.com>
2021-11-28 19:33:01 -0600

    Version1.47

--- app/src/main/java/com/example/proyectomc_dispmov/Olvdiarcontrasena2.kt
+++ app/src/main/java/com/example/proyectomc_dispmov/Olvdiarcontrasena2.kt
@@ -1,11 +1,19 @@
 package com.example.proyectomc_dispmov
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_olvdiarcontrasena2.*
 
 class Olvdiarcontrasena2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_olvdiarcontrasena2)
+
+        ok.setOnClickListener {
+            val intent = Intent(this, Autentificacion::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
