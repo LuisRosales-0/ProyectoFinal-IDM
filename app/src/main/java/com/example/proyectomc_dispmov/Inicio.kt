c9369703a9a53108a6bb7e64820627b076465ae5
LuisRosaless <wicholr98@gmail.com>
2021-11-28 18:13:44 -0600

    Version1.4

--- app/src/main/java/com/example/proyectomc_dispmov/Inicio.kt
+++ app/src/main/java/com/example/proyectomc_dispmov/Inicio.kt
@@ -3,24 +3,20 @@ package com.example.proyectomc_dispmov
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_inicio.*
+import android.widget.ImageView
 
 class Inicio : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio)
 
-        Opcion1.setOnClickListener {
-            val intent:Intent = Intent(this, Seleccion2::class.java)
+        val imagenclick = findViewById<ImageView>(R.id.armaa)
+        imagenclick.setOnClickListener {
+            val intent = Intent(this, Armapc::class.java)
             startActivity(intent)
         }
-        Opcion2.setOnClickListener {
-            val intent:Intent = Intent(this, Seleccion1::class.java)
-            startActivity(intent)
-        }
-        config.setOnClickListener {
-            val intent:Intent = Intent(this, Configuracion::class.java)
-            startActivity(intent)
-        }
+
+
+
     }
 }
